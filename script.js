76f23d48965bbfcedb5a65bf8dd488d400039f30
lorenasandoval88 <lorenasandoval88@gmail.com>
2022-06-15 17:44:56 -0400

    catch error

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+fetch('https://reqres.in/api/users/23')
+    .then(res => {
+        if (res.ok) {
+            console.log("SUCCESS")
+        } else {
+            console.log("NO SUCCESS")
+        }
+        res.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR')) // error is not caught
 
+    // failure only shows up if there is a failure within fetch itself, not the api
